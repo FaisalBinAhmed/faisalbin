f5fac403436b1417d152d9c09b5585aac1afd1c3
Faisal Bin Ahmed <faisalbin@outlook.com>
2024-04-01 23:35:23 +0200

    about

---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,52 @@
+import Link from "next/link";
+
+const HeroSection = () => {
+	return (
+		<div className="flex w-full flex-col md:flex-row items-center gap-8 p-8">
+			<div className="h-32 w-64 bg-red-200"></div>
+			<div className="p-2">
+				<p className="text-xl">Hello, there! 👋</p>
+				<p className="py-4">Welcome to my personal website.</p>
+				<p>
+					I've recently finished my Master's of Informatics at Technical
+					University of Munich. I'm working full-time as a software engineer at
+					Brainlab. If you want to know more about me, head over to my{" "}
+					<Link href="/about">"About Me"</Link> page.
+				</p>
+				<p className="py-4">Don't forget to follow me on social sites:</p>
+				<div className="flex flex-row gap-4 flex-wrap">
+					<a href="https://twitter.com/FaisalBAhmed" target="_blank">
+						<img
+							className="w-10 grayscale hover:grayscale-0"
+							src="/assets/twitter.png"
+							title="Twitter"
+						/>
+					</a>
+					<a href="https://medium.com/@Faisalbin" target="_blank">
+						<img
+							className="w-10 grayscale hover:grayscale-0"
+							src="/assets/medium.png"
+							title="Medium"
+						/>
+					</a>
+					<a href="https://github.com/FaisalBinAhmed" target="_blank">
+						<img
+							className="w-10 grayscale hover:grayscale-0"
+							src="/assets/github.png"
+							title="Github"
+						/>
+					</a>
+					<a href="https://linkedin.com/in/faisalbin/" target="_blank">
+						<img
+							className="w-10 grayscale hover:grayscale-0"
+							src="/assets/linkedin.png"
+							title="LinkedIn"
+						/>
+					</a>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default HeroSection;
