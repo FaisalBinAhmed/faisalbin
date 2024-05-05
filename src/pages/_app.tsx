f121d141384a2ac63fe5e720945f7dbe7e45dbe5
Faisal Bin Ahmed <faisalbin@outlook.com>
2024-05-05 21:42:23 +0200

    head title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import { VercelProvider } from "@/contexts/VercelContext";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { JetBrains_Mono } from "next/font/google";
+import Head from "next/head";
 
 const jbm = JetBrains_Mono({
 	subsets: ["latin"]
@@ -13,6 +14,9 @@ export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<main
 			className={`flex h-screen flex-col md:flex-row items-center ${jbm.className}`}>
+			<Head>
+				<title>Faisal Bin Ahmed</title>
+			</Head>
 			<Analytics />
 			<VercelProvider>
 				<Sidebar />
