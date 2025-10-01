103967622eb00519dd6c0e8b99659d2033501f75
Faisal Bin Ahmed <faisalbin@outlook.com>
2025-10-01 18:01:45 +0200

    custom domain

--- src/api/vercel.ts
+++ src/api/vercel.ts
@@ -10,20 +10,20 @@ type Response = {
 
 export async function fetchDeploymentInfo() {
 	try {
-		const token = process.env.VERCEL_TOKEN;
-		const projectId = process.env.PROJECT_ID;
-		const response = await fetch(
-			`https://api.vercel.com/v5/now/deployments?projectId=${projectId}&limit=1&state=READY`,
-			{
-				headers: {
-					Authorization: `Bearer ${token}`
-				}
-			}
-		);
-		const data = (await response.json()) as Response;
-		if (data?.deployments?.length > 0) {
-			return data.deployments[0];
-		}
+		// const token = process.env.VERCEL_TOKEN;
+		// const projectId = process.env.PROJECT_ID;
+		// const response = await fetch(
+		// 	`https://api.vercel.com/v5/now/deployments?projectId=${projectId}&limit=1&state=READY`,
+		// 	{
+		// 		headers: {
+		// 			Authorization: `Bearer ${token}`
+		// 		}
+		// 	}
+		// );
+		// const data = (await response.json()) as Response;
+		// if (data?.deployments?.length > 0) {
+		// 	return data.deployments[0];
+		// }
 		return null;
 	} catch (error) {
 		console.error("Error fetching deployment info:", error);
